0f43d5232c492876b0579c8fb14438da133142d2
Sungho Park <siminsungho@gmail.com>
2020-01-09 17:06:21 +0900

    Change EOL to Windows style and update libraries.
    * ubinos
        + Add t32 trace script for nucleof207zg
        + Fix bug of function "dtty_getc"
        + Add cmake option "UBINOS__BSP__STM32F2_RCC_HSE_CONFIG"
        + Modify to import the eclipse preprocessor option from a file

--- make/batch.mk
+++ make/batch.mk
@@ -16,25 +16,25 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myrtc2_nrf52dk
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myrtc_nrf52dk
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky2_nrf52dk
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky_nrf52dk
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky2_c_nrf52dk
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_blinky_c_nrf52dk
 	@echo ""
 	@echo ""
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_multilink_central2_nrf52dk
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5example/app CONFIG_NAME=myble_app_multilink_central_nrf52dk
 	@echo ""
 	@echo ""
 
