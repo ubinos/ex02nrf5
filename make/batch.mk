c14dafd89786e068e7e6a72fa6834fdb0db42c8c
Sungho Park <siminsungho@gmail.com>
2020-09-26 23:49:06 +0900

    change default board to nrf52840dk

--- make/batch.mk
+++ make/batch.mk
@@ -8,14 +8,6 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_NAME=ex02nrf5_nrf52dk
-	@echo ""
-	@echo ""
-
-
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
 	make -f makefile.mk $@ CONFIG_NAME=ex02nrf5_nrf52840dk
 	@echo ""
 	@echo ""
@@ -24,57 +16,7 @@
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myrtc_nrf52dk
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myrtc_nrf52dk_rtctick
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_blinky_nrf52dk
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_blinky_nrf52dk_rtctick
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_blinky_c_nrf52dk
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_blinky_c_nrf52dk_rtctick
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_multilink_central_nrf52dk
-	@echo ""
-	@echo ""
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=myble_app_multilink_central_nrf52dk_rtctick
-	@echo ""
-	@echo ""
-
-
-	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	@echo ""
-	@echo ""
-	make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52dk_seggerrtt
+	make -f makefile.mk $@ CONFIG_NAME=ex02nrf5_nrf52dk
 	@echo ""
 	@echo ""
 
