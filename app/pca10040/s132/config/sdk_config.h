9677e75f2e10ad47a98589117043f85b1613c870
Sungho Park <siminsungho@gmail.com>
2020-09-26 23:48:26 +0900

    fix build error

--- app/pca10040/s132/config/sdk_config.h
+++ app/pca10040/s132/config/sdk_config.h
@@ -7549,7 +7549,7 @@
  
 
 #ifndef NRF_CLI_ENABLED
-#define NRF_CLI_ENABLED 1
+#define NRF_CLI_ENABLED 0
 #endif
 
 // <o> NRF_CLI_ARGC_MAX - Maximum number of parameters passed to the command handler. 
