aa8de12a5c30eb93fb3730778fb113444014a901
Sungho Park <siminsungho@gmail.com>
2020-09-27 00:01:02 +0900

    refactoring for auto carriage return feature

--- app/ex02nrf5.c
+++ app/ex02nrf5.c
@@ -14,22 +14,22 @@ static void task2func(void *arg);
 int appmain(int argc, char *argv[]) {
 	int r;
 
-	printf("\n\n\n\r");
-	printf("================================================================================\n\r");
-	printf("ex02nrf5 (build time: %s %s)\n\r", __TIME__, __DATE__);
-	printf("================================================================================\n\r");
-	printf("\n\r");
+	printf("\n\n\n");
+	printf("================================================================================\n");
+	printf("ex02nrf5 (build time: %s %s)\n", __TIME__, __DATE__);
+	printf("================================================================================\n");
+	printf("\n");
 
 	srand(time(NULL));
 
 	r = task_create(NULL, task1func, NULL, task_getmiddlepriority(), 0, "task1");
 	if (0 != r) {
-		logme("fail at task_create\r\n");
+		logme("fail at task_create\n");
 	}
 
 	r = task_create(NULL, task2func, NULL, task_getmiddlepriority(), 0, "task2");
 	if (0 != r) {
-		logme("fail at task_create\r\n");
+		logme("fail at task_create\n");
 	}
 
 	ubik_comp_start();
@@ -44,7 +44,7 @@ static void task1func(void *arg) {
 
 	for (unsigned int i = 0;; i++) {
 		delayms = (rand() % 10 + 1) * 200;
-		printf("1: hello world ! (%u) (delay = %4d ms)\r\n", i, delayms);
+		printf("1: hello world ! (%u) (delay = %4d ms)\n", i, delayms);
 		task_sleepms(delayms);
 	}
 }
@@ -56,7 +56,7 @@ static void task2func(void *arg) {
 
 	for (unsigned int i = 0;; i++) {
 		delayms = (rand() % 10 + 1) * 200;
-		printf("2: hello world ! (%u) (delay = %4d ms)\r\n", i, delayms);
+		printf("2: hello world ! (%u) (delay = %4d ms)\n", i, delayms);
 		task_sleepms(delayms);
 	}
 }
