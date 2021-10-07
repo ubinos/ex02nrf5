3fa262fa0a17c3722dad6c84e4aa02614f72b090
Sungho Park <siminsungho@gmail.com>
2021-10-07 15:21:34 +0900

    refactoring

--- app/ex02nrf5_nrf52840dk.cmake
+++ app/ex02nrf5_nrf52840dk.cmake
@@ -11,21 +11,25 @@ set(APP__NAME "ex02nrf5")
 
 set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_ORIGIN 0x20003400 STRING)
 set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_LENGTH 0x0003CC00 STRING)
+
 set_cache(NRF5SDK__SWI_DISABLE0 TRUE BOOL)
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52840dk_softdevice.cmake)
 include(${PROJECT_LIBRARY_DIR}/nrf5sdk_wrapper/config/nrf5sdk.cmake)
+include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
 
-get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+string(TOLOWER ${UBINOS__BSP__BOARD_MODEL} _temp_board_model)
 string(TOLOWER ${UBINOS__BSP__NRF52_SOFTDEVICE_NAME} _temp_softdevice_name)
-string(TOLOWER ${NRF5SDK__BOARD_NAME} _temp_board_name)
 
-include_directories(${_tmp_source_dir}/arch/arm/cortexm/nrf5/${_temp_board_name}/${_temp_softdevice_name}/config)
+include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model}/${_temp_softdevice_name}/config)
+include_directories(${_tmp_source_dir}/arch/arm/cortexm/${_temp_board_model})
 include_directories(${_tmp_source_dir})
 
 file(GLOB_RECURSE _tmp_sources
     "${_tmp_source_dir}/*.c"
     "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.cc"
     "${_tmp_source_dir}/*.S"
     "${_tmp_source_dir}/*.s")
 
--- app/ex02nrf5_nrf52dk.cmake
+++ app/ex02nrf5_nrf52dk.cmake
@@ -11,10 +11,12 @@ set(APP__NAME "ex02nrf5")
 
 set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_ORIGIN 0x20003400 STRING)
 set_cache(UBINOS__BSP__LINK_MEMMAP_RAM_LENGTH 0x0000CC00 STRING)
+
 set_cache(NRF5SDK__SWI_DISABLE0 TRUE BOOL)
 
 include(${PROJECT_UBINOS_DIR}/config/ubinos_nrf52dk_softdevice.cmake)
 include(${PROJECT_LIBRARY_DIR}/nrf5sdk_wrapper/config/nrf5sdk.cmake)
+include(${PROJECT_LIBRARY_DIR}/nrf5sdk_extension/config/nrf5sdk_extension.cmake)
 
 get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 string(TOLOWER ${UBINOS__BSP__NRF52_SOFTDEVICE_NAME} _temp_softdevice_name)
