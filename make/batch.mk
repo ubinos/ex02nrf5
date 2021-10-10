d1a6dbef5187501527519beba20a4a3ed2a64df4
Sungho Park <siminsungho@gmail.com>
2021-10-11 01:13:52 +0900

    add library, esp8266at

--- make/batch.mk
+++ make/batch.mk
@@ -5,70 +5,70 @@
 	@echo ""
 
 
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=ex02nrf5_nrf52840dk
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=ex02nrf5_nrf52dk
-	# @echo ""
-	# @echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=ex02nrf5_nrf52840dk
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=ex02nrf5_nrf52dk
+	@echo ""
+	@echo ""
 
 
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52dk_seggerrtt
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52840dk_seggerrtt
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52840dongle_seggerrtt
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_ardnano33ble_seggerrtt
-	# @echo ""
-	# @echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52dk_seggerrtt
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52840dk_seggerrtt
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_nrf52840dongle_seggerrtt
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/seggerrtt_wrapper/app CONFIG_NAME=cli_tester_ardnano33ble_seggerrtt
+	@echo ""
+	@echo ""
 
 
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52840dk
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52840dk_rtctick
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52dk
-	# @echo ""
-	# @echo ""
-	# @echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-	# @echo ""
-	# @echo ""
-	# make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52dk_rtctick
-	# @echo ""
-	# @echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52840dk
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52840dk_rtctick
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52dk
+	@echo ""
+	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=ble_app_att_mtu_throughput_nrf52dk_rtctick
+	@echo ""
+	@echo ""
 
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
@@ -105,6 +105,14 @@
 	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../library/nrf5sdk_wrapper/app CONFIG_NAME=cli_tester_nrf52dk_rtctick
+	@echo ""
+	@echo ""
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../library/esp8266at/app CONFIG_NAME=esp8266at_tester_nrf52840dk
 	@echo ""
 	@echo ""
 
